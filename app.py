5b531796d1b7412fcbfe413aaaec5d52d0e6906b
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2020-12-19 19:42:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,19 +32,20 @@ lis_check = [{'label': col, 'value': col} for col in col_mul if col != col_mul[0
 st.write("Data contained into the dataset:", table)
 
 # map-box part
-st.write("Map")
+st.header("Map")
 
-st.sidebar.selectbox("select the nut column", table.columns, 0)
-st.sidebar.selectbox("select the feature column", col_mul, 0)
-st.sidebar.number_input("insert the quantile value", 0, 100, 50)
+st.sidebar.subheader("Map area")
+nut_col = st.sidebar.selectbox("select the nut column", table.columns, 0)
+map_feature = st.sidebar.selectbox("select the feature column", col_mul, 0)
+map_q = st.sidebar.number_input("insert the quantile value", 0, 100, 50)
 
 px.set_mapbox_access_token("pk.eyJ1IjoibHVjYXVyYmFuIiwiYSI6ImNrZm5seWZnZjA5MjUydXBjeGQ5ZDBtd2UifQ.T0o-wf5Yc0iTSeq-A9Q2ww")
-map_box = px.choropleth_mapbox(table, geojson = eu_nut2, locations = table['Nuts'], featureidkey = 'properties.id',
-                           color = 'FOUND_YEAR', color_continuous_scale = px.colors.cyclical.IceFire,
-                           range_color = (table['FOUND_YEAR'].min(), table['FOUND_YEAR'].quantile(0.95)),
+map_box = px.choropleth_mapbox(table, geojson = eu_nut2, locations = table[nut_col], featureidkey = 'properties.id',
+                           color = map_feature, color_continuous_scale = px.colors.cyclical.IceFire,
+                           range_color = (table[map_feature].min(), table[map_feature].max()),
                            mapbox_style = "carto-positron",
                            zoom = 3, center = {"lat": 47.4270826, "lon": 15.5322329},
                            opacity = 0.5,
-                           labels = {'FOUND_YEAR': 'FOUND_YEAR'})
+                           labels = {map_feature: map_feature})
 
 st.plotly_chart(map_box, use_container_width=True)
