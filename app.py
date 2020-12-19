be3be5f75264f4c696c2f7ddd7329ed92f041e99
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2020-12-19 18:15:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-pip install plotly
-
 import streamlit as st
 import time
 import base64
@@ -11,7 +9,7 @@ import numpy as np
 from pandas.api.types import is_numeric_dtype
 import json
 import random
-import plotly.figure_factory as ff
+import plotly
 
 # importing the table and all other necessary files
 table = pd.read_csv("https://raw.github.com/LucaUrban/prova_streamlit/main/table_final.csv")
