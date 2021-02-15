42e8b437cd3f01939b236f2e5baceb42643a929f
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2021-02-15 11:24:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -99,39 +99,44 @@ st.plotly_chart(multi_plot, use_container_width=True)
 # time control charts
 el_id = st.selectbox("element ID for time control chart", table[multi_index].unique(), 1)
 
-dff_tcc = table[table[multi_index] == el_id][[multi_time, multiXax_col]]
+dff_tcc = table[table[multi_index] == el_id][[multi_time, multiXax_col, multiYax_col]]
 if len(list(dff_tcc[multi_time].unique())) < dff_tcc.shape[0]:
-    res = {multi_time: [], multiXax_col: []}
+    res = {multi_time: [], multiXax_col: [], multiYax_col: []}
     for el in list(dff_tcc[multi_time].unique()):
-        res[multi_time].append(el); res[multiXax_col].append(dff_tcc[dff_tcc[multi_time] == el][multiXax_col].mean())
+        res[multi_time].append(el)
+        res[multiXax_col].append(dff_tcc[dff_tcc[multi_time] == el][multiXax_col].mean())
+        res[multiYax_col].append(dff_tcc[dff_tcc[multi_time] == el][multiYax_col].mean())
     dff_tcc = pd.DataFrame(data = res)
-title = '<b>{}</b><br>{}'.format(el_id, multiXax_col)
-fig = go.Figure()
+titleX = '<b>{}</b><br>{}'.format(el_id, multiXax_col); titleY = '<b>{}</b><br>{}'.format(el_id, multiYax_col)
 
-if dff_tcc.shape[0] != 0:
-    x_bar = []
-    for inst in table[multi_index].unique():
-        inst_data = table[table[multi_index] == inst][list(dff_tcc)[1]]
-        if inst_data.count() != 0:
-            x_bar.append(inst_data.mean())
+fig_tcc = make_subplots(rows = 2, cols = 1, specs = [[{}], [{}]], subplot_titles = (titleX, titleY))
+for i in range(2):
+    fig = go.Figure()
+    if dff_tcc.shape[0] != 0:
+        x_bar = []
+        for inst in table[multi_index].unique():
+            inst_data = table[table[multi_index] == inst][list(dff_tcc)[1]]
+            if inst_data.count() != 0:
+                x_bar.append(inst_data.mean())
 
-    x_barbar = round(sum(x_bar)/len(x_bar), 3)
+        x_barbar = round(sum(x_bar)/len(x_bar), 3)
 
-    x_LCL = x_barbar - (1.88 * (dff_tcc[list(dff_tcc)[1]].quantile(0.95) - dff_tcc[list(dff_tcc)[1]].quantile(0.05)))
-    x_UCL = x_barbar + (1.88 * (dff_tcc[list(dff_tcc)[1]].quantile(0.95) - dff_tcc[list(dff_tcc)[1]].quantile(0.05)))
+        x_LCL = x_barbar - (1.88 * (dff_tcc[list(dff_tcc)[i]].quantile(0.95) - dff_tcc[list(dff_tcc)[i]].quantile(0.05)))
+        x_UCL = x_barbar + (1.88 * (dff_tcc[list(dff_tcc)[i]].quantile(0.95) - dff_tcc[list(dff_tcc)[i]].quantile(0.05)))
 
-    x_el = [i for i in range(int(dff_tcc[multi_time].min()), int(dff_tcc[multi_time].max()) + 1)]
-    fig.add_trace(go.Scatter(x = dff_tcc[multi_time], y = dff_tcc[list(dff_tcc)[1]], mode = 'lines+markers', name = "Value"))
-    fig.add_trace(go.Scatter(x = x_el, y = [x_UCL for _ in range(len(x_el))], mode = "lines", name = "Upper Bound"))
-    fig.add_trace(go.Scatter(x = x_el, y = [x_LCL for _ in range(len(x_el))], mode = "lines", name = "Lower Bound"))
-    fig.update_xaxes(showgrid = False)
-    fig.add_annotation(x=0, y=0.85, xanchor='left', yanchor='bottom',
-                       xref='paper', yref='paper', showarrow=False, align='left',
-                       bgcolor='rgba(255, 255, 255, 0.5)', text = title)
-    fig.update_layout(xaxis_title = multi_time, yaxis_title = list(dff_tcc)[1])
-    fig.update_layout(height = 245, margin = {'l': 20, 'b': 30, 'r': 10, 't': 10})
+        x_el = [i for i in range(int(dff_tcc[multi_time].min()), int(dff_tcc[multi_time].max()) + 1)]
+        fig.add_trace(go.Scatter(x = dff_tcc[multi_time], y = dff_tcc[list(dff_tcc)[1]], mode = 'lines+markers', name = "Value"))
+        fig.add_trace(go.Scatter(x = x_el, y = [x_UCL for _ in range(len(x_el))], mode = "lines", name = "Upper Bound"))
+        fig.add_trace(go.Scatter(x = x_el, y = [x_LCL for _ in range(len(x_el))], mode = "lines", name = "Lower Bound"))
+        fig.update_xaxes(showgrid = False)
+        fig.add_annotation(x=0, y=0.85, xanchor='left', yanchor='bottom',
+                           xref='paper', yref='paper', showarrow=False, align='left',
+                           bgcolor='rgba(255, 255, 255, 0.5)', text = title)
+        fig.update_layout(xaxis_title = multi_time, yaxis_title = list(dff_tcc)[i])
+        fig.update_layout(height = 245, margin = {'l': 20, 'b': 30, 'r': 10, 't': 10})
+    fig_tcc.add_trace(fig)
 
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig_tcc, use_container_width=True)
 
 # pareto chart with feature importance on huber regressor
 st.header("Feature Importance Analysis")
