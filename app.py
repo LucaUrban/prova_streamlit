f4cf2c50498db9257051b20961363aeab5b98d7a
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2021-06-25 20:12:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -380,7 +380,7 @@ if uploaded_file is not None:
         # forecasting
         if ch_model == 'AR':
             fig_forecasting.add_trace(go.Scatter(x = [max(list(data[time].unique())) + i for i in range(2)], 
-                                                 y = [res[ids.index(ch_id) : -1], AutoReg(res[ids.index(ch_id)], lags = 1).fit().predict(len(res), len(res))], 
+                                                 y = [res[ids.index(ch_id) : -1], AutoReg(res[ids.index(ch_id)], lags = 1).fit().predict(len(res[0]), len(res[0]))], 
                                                  mode = 'lines+markers', name = "Prediction"))
         
         if ch_model == 'MA':
