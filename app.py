33c512eeeeddcc5a6ec23d084d2b5f4dd1f95519
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2021-06-25 19:18:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -368,8 +368,8 @@ if uploaded_file is not None:
         
         
         # visual part
-        st.table(pd.DataFrame({'AR': mean_squared_error(pred_ar, res[:, res.shape[1]-1]), 
-                               'MA': mean_squared_error(pred_ma, res[:, res.shape[1]-1]), 
-                               'ARMA': mean_squared_error(pred_arma, res[:, res.shape[1]-1]), 
-                               'ARIMA': mean_squared_error(pred_arima, res[:, res.shape[1]-1])}, 
-                              index = ['MSE error']))
+        mse_mins = np.array([mean_squared_error(pred_ar, res[:, res.shape[1]-1]), mean_squared_error(pred_ma, res[:, res.shape[1]-1]),
+                             mean_squared_error(pred_arma, res[:, res.shape[1]-1]), mean_squared_error(pred_arima, res[:, res.shape[1]-1])])
+        st.table(pd.DataFrame(mse_mins, columns = ['AR', 'MA', 'ARMA', 'ARIMA'], index = ['MSE error']))
+         
+        el_id = st.selectbox("Choose the model you want to use to forecast the next periods", ['AR', 'MA', 'ARMA', 'ARIMA'], np.argmin(mse_min))
