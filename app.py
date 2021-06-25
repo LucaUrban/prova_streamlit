6fff757981782719be7aed9477fbe6fdce373af8
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2021-06-25 11:59:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -348,15 +348,15 @@ if uploaded_file is not None:
         # recurring forecasting
         if modality == "Recurring Forecast":
             fin_mod = 0; MSE_fin_mod = 99999999
-            for i in range(1, res.shape[0]):
+            for i in range(6, res.shape[0]):
                 pred_res = np.array([])
                 
-                for j in range(6, res.shape[1]):
+                for j in range(res.shape[1]):
                     model = AutoReg(res[j, 0:i], lags = 1).fit()
                     yhat = model.predict()
                     pred_res = np.append(pred_res, y_hat)
                 
-                st.write(pred_res)
+                
                     
         
         
