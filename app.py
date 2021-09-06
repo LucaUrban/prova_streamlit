095a455d6df67c6b354518e3b3074babb42ca2e6
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2021-09-06 17:42:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -154,10 +154,10 @@ if uploaded_file is not None:
         cou_sel = st.selectbox("Choose the id of the country you want to explore", ['All ids'] + uniques, 0)
         if cou_sel == 'All ids':
             if ratio_vio_sel2 == 'None':
-                fig_vio = px.violin(res_ratio, y = "R_1", x = 'Sel', box = True, points = 'suspectedoutliers')
+                fig_vio = px.violin(res_ratio, y = "R_1", box = True, points = 'suspectedoutliers')
             else:
                 res_ratio['Color'] = table[ratio_vio_sel2]
-                fig_vio = px.violin(res_ratio, y = "R_1", x = 'Sel', color = 'Color', box = True, points = 'suspectedoutliers')
+                fig_vio = px.violin(res_ratio, y = "R_1", color = 'Color', box = True, points = 'suspectedoutliers')
         else:
             if ratio_vio_sel2 == 'None':
                 fig_vio = px.violin(res_ratio[res_ratio['Sel'] == cou_sel], y = "R_1", x = 'Sel', box = True, points = 'suspectedoutliers')
