7a3454eb51cfbfa121e046c7096b1a09321fd7b9
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2021-09-21 08:46:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -761,8 +761,9 @@ if uploaded_file is not None:
                 DV_fin_res[inst[:2]] = 1
             else:
                 DV_fin_res[inst[:2]] += 1
+        st.write(DV_fin_res.values())
         DV_fin_tab = pd.DataFrame(np.array(DV_fin_res.values()).reshape(1, 16), columns = DV_fin_res.keys(), index = con_checks_features)
-        st.write(DV_fin_tab)
+        
         
         
         
