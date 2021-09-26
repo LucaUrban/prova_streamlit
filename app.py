fdde9ff21648806cf5c8bde3551fae20f3496b7d
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2021-09-26 08:41:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -781,8 +781,8 @@ if uploaded_file is not None:
             flags_col = st.selectbox("Select the specific category you want to analize", table.columns)
             
         ones = set(table[table[flags_col] == 1][con_checks_id_col].values); twos = set(table[table[flags_col] == 2][con_checks_id_col].values)
-        st.write(len(ones)); st.write(len(twos))
-        #st.table(pd.Dataframe([[], [], []], index = [''], columns = ['Accuracy respect the istitutions with confirmed ', '', '']))
+        st.table(pd.Dataframe([[0], [0], [0]], index = [''], 
+                              columns = ['Accuracy respect the confirmed cases', '#application cases vs. #standard cases', 'Number of not flagged cases']))
         
         
         
