c046af33b64823ae17d2d4e6c2f14c2c9e3463a5
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2021-09-26 09:02:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -784,7 +784,7 @@ if uploaded_file is not None:
             
         ones = set(table[table[flags_col] == 1][con_checks_id_col].values); twos = set(table[table[flags_col] == 2][con_checks_id_col].values)
         st.table(pd.DataFrame([[str(round((100 * len(twos.intersect(dict_check_flags[var_control_checks_flag]))) / len(twos), 2)) + '%'], 
-                               [0], [0]], columns = [''], 
+                               [str(len(dict_check_flags[var_control_checks_flag])) + ' / ' + str(len(ones.union(twos)))], [0]], columns = [''], 
                               index = ['Accuracy respect the confirmed cases', '#application cases vs. #standard cases', 'Number of not flagged cases']))
         
         
