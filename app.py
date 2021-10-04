81f243893c4f06430554ce075d99fc60024e60b5
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2021-10-04 12:23:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -833,7 +833,7 @@ if uploaded_file is not None:
         
         st.plotly_chart(fig_concistency, use_container_width=True)
         st.table(summ_table)
-        st.table(pd.DataFrame(DV_fin_res))
+        st.table(pd.DataFrame(DV_fin_res, index = con_checks_features + ['Total'], columns = list_countries + ['Total']))
         
         set_type = st.selectbox("Type of istitution's set:", ['-', '', '', 'Not flagged cases'])
         
