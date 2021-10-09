edebe346936910d2001c709f0e15c277a712e177
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2021-10-09 12:57:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -721,8 +721,8 @@ if uploaded_file is not None:
                         for row in range(len(list_fin_res)):
                             for i in range(len(list_fin_res[row])):
                                 if list_fin_res[row][len(list_fin_res[row])-1] != 0: 
-                                    st.write(num_app)
                                     num_app = list_fin_res[row][i]/list_fin_res[row][len(list_fin_res[row])-1]
+                                    st.write(num_app)
                                     list_fin_res[row][i] = str(list_fin_res[row][i]) + '\n(' + str(round(100 * num_app, 2)) + '%)'
                                     if i != len(list_fin_res[row])-1 and 100 * num_app >= prob_cases_per:
                                         list_prob_cases.append([con_checks_features[int(row // len(categories))], countries[i], categories[int(row % len(categories))]])
