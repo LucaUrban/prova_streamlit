7c44cec734599a58918a5726abc252932d54673c
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2021-11-03 10:40:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -728,9 +728,9 @@ if uploaded_file is not None:
                                     den = len(table[(table[country_sel_col] == countries[i]) & (table[cat_sel_col] == categories[row])][con_checks_id_col].unique())
                                     num = list_fin_res[row][i]
                                     if den != 0:
-                                        list_fin_res[row][i] = str(list_fin_res[row][i]) + '\n(' + str(round(100 * num/den, 2)) + '%)'
+                                        num_app = round(100 * num/den, 2); list_fin_res[row][i] = str(list_fin_res[row][i]) + '\n(' + str(num_app) + '%)'
                                     else:
-                                        list_fin_res[row][i] = '0\n(0%)'
+                                        num_app = 0; list_fin_res[row][i] = '0\n(0%)'
                                     if i != len(list_fin_res[row])-1 and num_app >= prob_cases_per:
                                         if row != len(list_fin_res)-1:
                                             list_prob_cases.append([con_checks_features[int(row // len(categories))], countries[i], categories[int(row % len(categories))], str(num_app) + '%', str(num) + ' / ' + str(den)])
