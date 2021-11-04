20406369a2f9fb2870211b9f2db346d67f2e8bcf
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2021-11-04 12:29:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ if demo_data or uploaded_file is not None:
     if uploaded_file is not None:
         table = pd.read_csv(uploaded_file)
     else:
-        table = pd.read_csv('https://github.com/LucaUrban/prova_streamlit/blob/main/eter_fin_ratio_.csv')
+        table = pd.read_csv('https://github.com/LucaUrban/prova_streamlit/eter_fin_ratio_.csv')
 
     # importing all other necessary files
     with urlopen('https://raw.githubusercontent.com/leakyMirror/map-of-europe/master/GeoJSON/europe.geojson') as response:
