ab3b3f4c8a87a58c9d5316076464ed820c5d0722
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2021-11-15 09:55:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -842,7 +842,7 @@ if demo_data or uploaded_file is not None:
 
             DV = dict() # the dictionary in wich we'll store all the DV and further the DM values for the variability from years
             for key, value in res.items():
-                res_par = 0
+                res_par = np.nan
                 if len(value[0]) != 0 and len(value[1]) != 0:
                     res_par = sum(value[0]) * sum(value[1])
                 if not np.isnan(indices[key[key.find('.')+1:]][key[:key.find('.')]]) and indices[key[key.find('.')+1:]][key[:key.find('.')]] != 0:
