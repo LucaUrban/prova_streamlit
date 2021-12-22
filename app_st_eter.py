44e1fd4f71baa66b3168b7f98307c1888ef159ca
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2021-12-22 09:27:31 +0100

    Update app_st_eter.py

--- app_st_eter.py
+++ app_st_eter.py
@@ -353,7 +353,7 @@ if demo_data_radio == 'Yes' or uploaded_file is not None:
         # visual part
         dis_fit = [[result_norm[1], result_exp[1], result_lognorm[1], result_weibull2[1]], 
                    [str(result_norm[1] > 0.05), str(result_exp[1] > 0.05), str(result_lognorm[1] > 0.05), str(result_weibull2[1] > 0.05)]]
-        st.table(pd.DataFrame(dis_fit, columns = ['Normal', 'Exponential', 'Log-Norm', 'Weibul'], index = ['P-value', 'P > t']))
+        st.dataframe(pd.DataFrame(dis_fit, columns = ['Normal', 'Exponential', 'Log-Norm', 'Weibul'], index = ['P-value', 'P > t']))
 
         ch_distr = st.selectbox("Choose the distribution you want to use for the anomalies estimation", ['Normal', 'Exponential', 'Log-Norm', 'Weibull'])
         fig_distr = go.Figure(data = [go.Histogram(x = var_clean, 
