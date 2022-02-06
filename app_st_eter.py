e5ad13e6717dee94088c995c2ddba1cf37c5e6d9
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2022-02-06 13:57:40 +0100

    Update app_st_eter.py

--- app_st_eter.py
+++ app_st_eter.py
@@ -33,7 +33,11 @@ demo_data_radio = st.radio("What is the dataset you want to import:", ('Demo dat
 if demo_data_radio == 'Demo datset' or uploaded_file is not None:
     if uploaded_file is not None:
         if demo_data_radio == 'ETER Dataset':
-            table = pd.read_csv(uploaded_file, delimiter = ';')
+            table = pd.read_csv(uploaded_file, delimiter = ';', decimal = ',')
+            
+            for col in table.columns:
+                if table[col].dtypes == 'O' and (col.startswith('Flag') or col.startswith('Notes')):
+                    table[col] = table[col].apply(lambda x: x.replace(',', '.') if not pd.isna(x) else x)
             
             for token in ['a', 'c', 'm',  'nc','s', 'x', 'xc', 'xr']:
                 table.replace({token: np.nan}, inplace = True)
