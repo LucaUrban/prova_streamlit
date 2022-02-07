3dcc42abbc4a7b1458b155c596d038dca712752e
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2022-02-07 17:23:48 +0100

    Update app_st_eter.py

--- app_st_eter.py
+++ app_st_eter.py
@@ -829,10 +829,10 @@ if demo_data_radio == 'Demo datset' or uploaded_file is not None:
             table.loc[table[table[con_checks_id_col].isin(dict_check_flags)].index, 'Prob inst ' + con_checks_features] = 1
                         
             if cat_sel_col == '-':
+                st.write(DV_fin_res)
                 DV_fin_res = np.array([np.append(DV_fin_res, np.array([np.sum(DV_fin_res, axis = 0)]), axis = 0)])
                 DV_fin_res = np.append(DV_fin_res, DV_fin_res, axis = 0)
                 list_fin_res = DV_fin_res.tolist()
-                st.write(list_fin_res)
                 for row in range(len(list_fin_res)):
                     for i in range(len(list_fin_res[row])):
                         list_fin_res[row][i] = str(list_fin_res[row][i]) + '\n(' + str(round(100 * (list_fin_res[row][i]/list_fin_res[row][len(list_fin_res[row])-1]), 2)) + '%)'
